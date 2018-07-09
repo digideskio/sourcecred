25d0c753da852a834ddd811202b3c2a2ba5bf415
William Chargin <wchargin@gmail.com>
2018-07-09 14:25:19 -0700

    reorg

--- src/app/credExplorer/PagerankTable.test.js
+++ src/app/credExplorer/PagerankTable.test.js
@@ -154,7 +154,7 @@ describe("app/credExplorer/PagerankTable", () => {
       );
       expect(enzymeToJSON(element)).toMatchSnapshot();
     });
-    it("throws an error if maxEntriesPerlist not set", () => {
+    it("throws an error if maxEntriesPerList not set", () => {
       const {pnd, adapters} = example();
       expect(() =>
         shallow(
@@ -167,9 +167,18 @@ describe("app/credExplorer/PagerankTable", () => {
         )
       ).toThrowError("maxEntriesPerList");
     });
+    it("renders thead column order properly", () => {
+      const {pnd, adapters} = example();
+      const element = shallow(
+        <PagerankTable pnd={pnd} adapters={adapters} maxEntriesPerList={1} />
+      );
+      const th = element.find("thead th");
+      const columnNames = th.map((t) => t.text());
+      expect(columnNames).toEqual(COLUMNS());
+    });
 
     describe("has a filter select", () => {
-      function filterSetup() {
+      function setup() {
         const {pnd, adapters} = example();
         const element = shallow(
           <PagerankTable pnd={pnd} adapters={adapters} maxEntriesPerList={1} />
@@ -179,7 +188,7 @@ describe("app/credExplorer/PagerankTable", () => {
         return {pnd, adapters, element, label, options};
       }
       it("with expected label text", () => {
-        const {label} = filterSetup();
+        const {label} = setup();
         const filterText = label
           .find("span")
           .first()
@@ -187,7 +196,7 @@ describe("app/credExplorer/PagerankTable", () => {
         expect(filterText).toMatchSnapshot();
       });
       it("with expected option groups", () => {
-        const {options} = filterSetup();
+        const {options} = setup();
         const optionsJSON = options.map((o) => ({
           valueString: NodeAddress.toString(o.prop("value")),
           style: o.prop("style"),
@@ -196,7 +205,7 @@ describe("app/credExplorer/PagerankTable", () => {
         expect(optionsJSON).toMatchSnapshot();
       });
       it("with the ability to filter nodes passed to NodeRowList", () => {
-        const {element, options} = filterSetup();
+        const {element, options} = setup();
         const option1 = options.at(1);
         const value = option1.prop("value");
         expect(value).not.toEqual(NodeAddress.empty);
@@ -212,18 +221,9 @@ describe("app/credExplorer/PagerankTable", () => {
         expect(actualNodes).not.toHaveLength(0);
       });
     });
-    it("renders thead column order properly", () => {
-      const {pnd, adapters} = example();
-      const element = shallow(
-        <PagerankTable pnd={pnd} adapters={adapters} maxEntriesPerList={1} />
-      );
-      const th = element.find("thead th");
-      const columnNames = th.map((t) => t.text());
-      expect(columnNames).toEqual(COLUMNS());
-    });
 
     describe("creates a NodeRowList", () => {
-      function setupNodeRowList() {
+      function setup() {
         const {adapters, pnd} = example();
         const maxEntriesPerList = 1;
         const element = shallow(
@@ -236,13 +236,13 @@ describe("app/credExplorer/PagerankTable", () => {
         const nrl = element.find("NodeRowList");
         return {adapters, pnd, element, nrl, maxEntriesPerList};
       }
-      it("NodeRowList is passed SharedProps appropriately", () => {
-        const {nrl, adapters, pnd, maxEntriesPerList} = setupNodeRowList();
+      it("with the correct SharedProps", () => {
+        const {nrl, adapters, pnd, maxEntriesPerList} = setup();
         const expectedSharedProps = {adapters, pnd, maxEntriesPerList};
         expect(nrl.prop("sharedProps")).toEqual(expectedSharedProps);
       });
-      it("by default, all nodes are passed to NodeRowList", () => {
-        const {nrl, pnd} = setupNodeRowList();
+      it("including all nodes by default", () => {
+        const {nrl, pnd} = setup();
         const expectedNodes = Array.from(pnd.keys());
         expect(nrl.prop("nodes")).toEqual(expectedNodes);
       });
